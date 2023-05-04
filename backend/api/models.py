e9a5abe7df9581d4dc19e87b61f1fdc5ff200643
Gleb Kovalev <kovalev@mindbox.cloud>
2023-05-04 11:04:34 +0300

    fix: pep8

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Task(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=120)
     description = models.TextField()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
+
 from api import views
 
 router = routers.DefaultRouter()
