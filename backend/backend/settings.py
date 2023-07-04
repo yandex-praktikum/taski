3d5767f6451f051dda70ea8c0936a32e538945bd
Max <115157301+maxfreym@users.noreply.github.com>
2023-07-04 17:21:34 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.20.40', '127.0.0.1', 'localhost']
 
 
 # Application definition
