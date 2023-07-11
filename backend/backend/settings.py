85ac4e95caf4ab2ccec20c7c6e8f8ea2fcda32d6
demetrius2017 <demetrius_i@mail.ru>
2023-07-11 17:49:45 +0300

    delete sentry_sdk

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,23 +1,5 @@
 import os
 from pathlib import Path
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://3587f37c4e724305a53e44188560425e@o4505453253820416.ingest.sentry.io/4505453271842816",
-    integrations=[
-        DjangoIntegration(),
-    ],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
